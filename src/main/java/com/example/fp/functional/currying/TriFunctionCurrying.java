f2ede0b956b6b1efca413793762e39e632367e81
Sameer Shukla <sameershukla@Sameers-MBP.attlocal.net>
2023-02-19 03:28:22 -0600

    Updated README

--- src/main/java/com/example/fp/functional/currying/TriFunctionCurrying.java
+++ src/main/java/com/example/fp/functional/currying/TriFunctionCurrying.java
@@ -9,11 +9,6 @@ import java.util.function.Function;
 public class TriFunctionCurrying {
 
 
-
-    private static Function<String, Tuple<String, String>> concatenate(String f, String m, Function<String, Function<String, Function<String, Tuple<String, String>>>> func){
-         return func.apply(f).apply(m);
-    }
-
     private static Tuple<String, String> removeCharacters(String a, String b) {
         return new Tuple<String, String>(a.replaceAll("[^\\p{Alpha}]+", " "), b.replaceAll("[^\\p{Alpha}]+", " "));
     }
@@ -31,10 +26,6 @@ public class TriFunctionCurrying {
           Tuple<String, String> result = p.andThen(x -> capitalize(x._1(), x._2())).andThen(x -> removeCharacters(x._1(), x._2()))
                   .apply(tuple._1(), tuple._2());
           System.out.println(result);
-//        Function<String, Function<String, Function<String, Tuple<String, String>>>> p = (f) -> (m) -> (l) -> new Tuple<>(f, m + " "+ l);
-//        Tuple<String, String> r = p.apply("Java").apply(" Programming").apply("Language");
-//        System.out.println(r._1() + r._2());
-
     }
 
 }
