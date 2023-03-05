4aad07e3c642f059da517a94466456b59f14e2bd
Sameer Shukla <sameershukla@Sameers-MBP.attlocal.net>
2023-03-04 23:59:55 -0600

    Functional Rest Client

---
+++ src/main/java/com/example/fp/problems/fileio/FunctionalRestClient.java
@@ -0,0 +1,68 @@
+package com.example.fp.problems.fileio;
+
+import com.example.fp.basics.types.Tuple;
+import com.example.fp.basics.types.Unit;
+import lombok.Getter;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Scanner;
+import java.util.function.Function;
+
+public class FunctionalRestClient {
+
+    @Getter
+    class Response{
+       public Boolean format;
+       public String domain;
+       public Boolean disposable;
+       public Boolean dns;
+       public Boolean whitelist;
+    }
+
+    private static HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+
+    private static Function<String, Tuple<HttpClient, HttpRequest>> createRequest = (url) -> {
+        return new Tuple<>(httpClient, HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create(url))
+                .build());
+    };
+
+    private static  HttpResponse<String> executeRequest(Tuple<HttpClient, HttpRequest> tuple){
+        try {
+            HttpResponse<String> response = tuple._1().send(tuple._2(), HttpResponse.BodyHandlers.ofString());
+            return response;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    private static Function<HttpResponse<String>, Unit> executeResponse(HttpResponse<String> response)  {
+        System.out.println(response.statusCode() + "-" + response.body());
+        return (t) -> Unit.unit();
+    };
+
+    public static void main(String[] args) {
+        System.out.println("Enter EmailId :");
+        String email = new Scanner(System.in).nextLine();
+        if(email == null){
+            throw new IllegalArgumentException("Email Cannot be Null");
+        }
+        String url = "https://www.disify.com/api/email/" + email;
+        createRequest
+                .andThen(FunctionalRestClient :: executeRequest)
+                .andThen(FunctionalRestClient :: executeResponse)
+                .apply(url);
+    }
+
+
+}
--- src/main/java/com/example/fp/problems/fileio/JdbcPipeline.java
+++ src/main/java/com/example/fp/problems/fileio/JdbcPipeline.java
@@ -1,2 +1,71 @@
-package com.example.fp.problems.fileio;public class JdbcPipeline {
+package com.example.fp.problems.fileio;
+
+import com.example.fp.basics.types.Tuple;
+import com.example.fp.basics.types.Unit;
+
+import java.sql.*;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class JdbcPipeline {
+
+    private static Function<Unit, Connection> createConnection = (u) -> {
+        String url = "jdbc:h2:mem:testdb";
+        String user = "sa";
+        String password = "password";
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    };
+
+    private static Tuple<Connection, ResultSet> executeQuery(Connection conn, String query){
+            try {
+                Statement statement = conn.createStatement();
+                ResultSet rs = statement.executeQuery(query);
+                statement.close();
+                return new Tuple<>(conn, rs);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+    };
+
+    private static Tuple<Connection, ResultSet> executeResultSet(Tuple<Connection, ResultSet> tuple) {
+        ResultSet rs = tuple._2();
+        try{
+            while(rs.next()){
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                int age = rs.getInt("age");
+                String department = rs.getString("department");
+                double salary = rs.getDouble("salary");
+                System.out.println("ID: " + id + ", Name: " + name + ", Age: " + age + ", Department: " + department + ", Salary: " + salary);
+            }
+        }catch(SQLException ex){
+            throw new RuntimeException(ex);
+        }
+        return tuple;
+    };
+
+    private static Unit closeConnection(Tuple<Connection, ResultSet> tuple){
+        try {
+            tuple._1().close();
+            tuple._2().close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return Unit.unit();
+    }
+
+
+    public static void main(String[] args) {
+         createConnection
+                 .andThen(x -> executeQuery(x, "SELECT * FROM Employee"))
+                 .andThen(tuple -> executeResultSet(tuple))
+                 .andThen(tuple -> closeConnection(tuple))
+                 .apply(Unit.unit());
+    }
+
+
 }
